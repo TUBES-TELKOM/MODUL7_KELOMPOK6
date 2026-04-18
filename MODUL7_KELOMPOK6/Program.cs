28bf57e9fd53520e202e0b2be1b88f463bd71cba
unknown <zoovasoup@proton.me>
2026-04-18 08:36:38 +0700

    [feat] watchlist class and read

--- MODUL7_KELOMPOK6/Program.cs
+++ MODUL7_KELOMPOK6/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Collections.Generic;
+using MODUL7_KELOMPOK6;
+
+namespace TP_MODUL7_KELOMPOK6
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WatchList wl = new WatchList();
+            wl.ReadJson();
+
+        }
+    }
+}
